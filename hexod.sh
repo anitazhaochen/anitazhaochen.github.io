ee1e066ffedcc09550d83316ac2a97e3526771b5
zhaochenwindows <zhaochen@email.com>
2020-05-26 00:47:51 +0800

    hexod bug修复

--- hexod.sh
+++ hexod.sh
@@ -12,8 +12,9 @@ echo "开始生成静态文件"
 echo "准备开始发布"
 
 echo "正在提交 GitLab 备份"
-`git pull origin && hexo g`
-`git add . && git commit -m "$1" && git push origin`
+git pull origin dev
+hexo g
+git add . && git commit -m "$1" && git push origin dev
 echo "提交到dev分支完成"
 
 
