9f8485e8bbed455d350ca4c3c6cd766854ab213e
zhaochen <zhaochen@>
2020-07-01 11:15:11 +0800

    更新脚本

--- hexod.sh
+++ hexod.sh
@@ -12,9 +12,7 @@ echo "开始生成静态文件"
 echo "准备开始发布"
 
 echo "正在提交 GitLab 备份"
-git pull origin dev
-hexo g
-git add . && git commit -m "$1" && git push origin dev
+git pull origin dev && hexo g && git add . && git commit -m "$1" && git push origin dev
 echo "提交到dev分支完成"
 
 
