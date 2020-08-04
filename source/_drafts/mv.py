4fdfa059b9cc47af2edc3d761905a6adadafd17d
zhaochen <zhaochen@>
2020-08-04 12:10:49 +0800

    自动发布博客

--- source/_drafts/mv.py
+++ source/_drafts/mv.py
@@ -6,10 +6,10 @@ import os
 if __name__ == "__main__":
     args = sys.argv
     if len(args) < 2:
-        print "缺少文章题目参数"
+        print("缺少文章题目参数")
         exit(-1)
     else:
-        print sys.argv[1]
+        print(sys.argv[1])
         with open(sys.argv[1],'r') as f:
             li = f.readlines()
             li.insert(20,"<!--more -->\n")
@@ -17,4 +17,4 @@ if __name__ == "__main__":
                 f.writelines(li)
         os.popen(r"sed  's/\(^.*!\[.*\)\.\.\(.*\)/\1\2/g' %s > ../_posts/%s"%(sys.argv[1], sys.argv[1]))
         os.popen("mv %s %s.bak"%(sys.argv[1],sys.argv[1]))
-        print "已发布完成"
+        print("已发布完成")
